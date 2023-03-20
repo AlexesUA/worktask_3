a5c0e1fa4b9e34b3d53e5859216c230e3a0797a9
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-03-20 20:38:23 +0200

    Relise Inpun & Output

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scInput = new Scanner(System.in);
+        double number1, number2, rezult = 0;
+        char operand;
+        System.out.print("Введіть перше число: ");
+        number1 = scInput.nextDouble();
+
+        System.out.print("Введіть друге число: ");
+        number2 = scInput.nextDouble();
+
+        System.out.print("Введіть математичну операцію: ");
+        operand = scInput.next().charAt(0);
+
+        //Зробити логіку калькулятора
+
+        System.out.printf("Результат рівняння %1$s %2$s %3$s = %4$s %n", number1, operand, number2, rezult);
+    }
+}
